7cff87c9d9daea5afca696eacc86b5b6fa5890c9
lukasweibel <lukasweibel@gmx.ch>
2018-12-14 09:20:00 +0100

    Implements Division

--- src/main/java/ch/bbw/Calculator.java
+++ src/main/java/ch/bbw/Calculator.java
@@ -1,11 +1,15 @@
 package ch.bbw;
 
 public class Calculator {
-    public int summe(int summand1, int summand2){
-        return summand1 + summand2;
+    public int summe(int value1, int value2) {
+        return value1 + value2;
     }
 
-    public int subtraktion(int value1, int value2){
+    public int subtraktion(int value1, int value2) {
         return value1 - value2;
+    }
+
+    public int division(int value1, int value2) {
+        return value1 / value2;
     }
 }
