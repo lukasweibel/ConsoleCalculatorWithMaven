dc265255cdcf174a876b682259a578c130503043
lukasweibel <lukasweibel@gmx.ch>
2018-12-14 13:43:35 +0100

    Implements addition tests

--- src/test/java/ch/bbw/CalculatorTest.java
+++ src/test/java/ch/bbw/CalculatorTest.java
@@ -15,17 +15,59 @@ public class CalculatorTest {
         visibilityTest = new VisibilityTest();
     }
 
+    //Tests class "addition"
     @Test
-    public void testSummeZweiPositiveIsOk() {
-        assertTrue(calculator.summe(10, 25) == 35);
+    public void testAdditionTwoPositiveNumbers() {
+        assertTrue(calculator.addition(10, 25) == 35);
     }
 
     @Test
-    public void testSubtractionZweiPositiveIsOk() {
-        assertTrue(calculator.subtraktion(25, 10) == 15);
+    public void testAdditionPositiveAndNegativeNumber() {
+        assertTrue(calculator.addition(10, -2) == 8);
     }
 
+    @Test
+    public void testAdditionPositiveNumberAndNumberZero() {
+        assertTrue(calculator.addition(10, 0) == 10);
+    }
 
+    @Test
+    public void testAdditionTwoNegativeNumbers() {
+        assertTrue(calculator.addition(-10, -10) == -20);
+    }
+
+    @Test
+    public void testAdditionResultIsOverMax() {
+        assertTrue(calculator.addition(Integer.MAX_VALUE, 3) == Integer.MIN_VALUE + 2);
+    }
+
+    @Test
+    public void testAdditionResultIsUnderMax() {
+        assertTrue(calculator.addition(Integer.MIN_VALUE, -25) == Integer.MAX_VALUE - 24);
+    }
+
+    @Test
+    public void testAdditionMaxAndMin() {
+        assertTrue(calculator.addition(Integer.MAX_VALUE, Integer.MIN_VALUE) == -1);
+    }
+
+    @Test
+    public void testAdditionResultIsNegative() {
+        assertTrue(calculator.addition(-4, -4) == -8);
+    }
+
+    //Tests class "subtraktion"
+    @Test
+    public void testSubtractionTwoPositiveNumbers() {
+        assertTrue(calculator.subtraction(25, 10) == 15);
+    }
+
+    @Test
+    public void testSubtractionPositiveAndNegativeNumber() {
+        assertTrue(calculator.subtraction(25, 10) == 15);
+    }
+
+    //Tests class "division"
     @Test
     public void testDivisionZweiPositiveIsOk() {
         assertTrue(calculator.division(50, 10) == 5);
@@ -36,10 +78,10 @@ public class CalculatorTest {
         assertTrue(calculator.division(10, 0) == 0);
     }
 
-    @Test
-    public void testDivisionDurch0ThrowsUnexpectedException() throws ArithmeticException {
-        assertTrue(calculator.division(10, 0) == 0);
-    }
+    // @Test
+    //public void testDivisionDurch0ThrowsUnexpectedException() throws ArithmeticException {
+    //   assertTrue(calculator.division(10, 0) == 0);
+    //}
 
 
     //Tests with different Visabilities
